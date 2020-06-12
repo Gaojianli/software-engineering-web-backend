e3bde1994df46759568ce51e1173fc70da673ce9
Gaojianli <823805669hehe@gmail.com>
2020-06-12 20:24:36 +0800

    add sql

---
+++ web-backend/init.sql
@@ -0,0 +1,45 @@
+﻿-- --------------------------------------------------------
+-- 主机:                           127.0.0.1
+-- 服务器版本:                        10.4.13-MariaDB - mariadb.org binary distribution
+-- 服务器操作系统:                      Win64
+-- HeidiSQL 版本:                  11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- 导出 acctrl 的数据库结构
+CREATE DATABASE IF NOT EXISTS `acctrl` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `acctrl`;
+
+-- 导出  表 acctrl.order 结构
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `roomID` int(10) unsigned NOT NULL,
+  `checkInTime` datetime DEFAULT current_timestamp(),
+  `checkOutTime` datetime DEFAULT NULL,
+  `fee` double unsigned zerofill NOT NULL DEFAULT 0000000000000000000000,
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- 数据导出被取消选择。
+
+-- 导出  表 acctrl.user 结构
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` varchar(10) NOT NULL DEFAULT 'manager',
+  PRIMARY KEY (`id`),
+  KEY `username` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- 数据导出被取消选择。
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
