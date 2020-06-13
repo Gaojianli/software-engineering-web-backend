4a2dff0495c5e4f9a9dc0a8e01ddb5b287f75046
Gaojianli <823805669hehe@gmail.com>
2020-06-13 15:30:50 +0800

    use for each

--- web-backend/Service/FeeService.cs
+++ web-backend/Service/FeeService.cs
@@ -12,21 +12,16 @@ namespace web_backend.Service
         {
             var requests = await ACServices.getControllRequest(roomID, dbContext);
             requests.OrderBy(s => s.time);
-            IEnumerator<ControllRequest> i = requests.GetEnumerator();
-
             float fee = 0;
-
-            while (i.MoveNext())
+            foreach(var cur in requests)
             {
-                ControllRequest cur = i.Current;
-                if(cur.status == false) continue;
-                if(System.Math.Abs((float)(cur.targetTemp - cur.nowTemp)) < 0.01) //温度稳定
+                if (cur.status == false) continue;
+                if (System.Math.Abs((float)(cur.targetTemp - cur.nowTemp)) < 0.01) //温度稳定
                     fee += (float)0.5;
-                else if(cur.fanSpeed <= 200) fee += (float)1;
-                else if(cur.fanSpeed <= 400) fee += (float)0.5;
+                else if (cur.fanSpeed <= 200) fee += (float)1;
+                else if (cur.fanSpeed <= 400) fee += (float)0.5;
                 else fee += (float)0.3333333;
             }
-
             return fee;
         }
     }
