d7b02d82b36496cdc14519e3f6c85ce0ba99fd47
Gaojianli <823805669hehe@gmail.com>
2020-06-14 13:50:01 +0800

    fixed

--- web-backend/Controllers/RoomController.cs
+++ web-backend/Controllers/RoomController.cs
@@ -107,6 +107,7 @@ namespace web_backend.Controllers
                             msg = "Accepted."
                         });                        
                     } else {
+                        await ACServices.changeStatusAsync(id, false, mode, targetTemp, fanSpeed, nowTemp, dbContext);
                         Response.StatusCode = 503;
                         return new JsonResult(new
                         {
